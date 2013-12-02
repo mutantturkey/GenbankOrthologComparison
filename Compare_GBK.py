f66b4fd07ff1b060bdc719407bc9c4f26e235126
Calvin Morrison <mutantturkey@gmail.com>
2013-12-02 11:23:52 -0500

    cleanup code, and take arguments in the same format as our Create Table script does

--- Compare_GBK.py
+++ Compare_GBK.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python
+
 import argparse
 from Bio import SeqIO
 from Bio.SeqFeature import FeatureLocation
 from Bio.SeqFeature import ExactPosition
+
 import os
 import csv
 import sys
-import pdb
 
 def main():
-	if(len(sys.argv) != 5):
-		raise Exception("you need four arguments: ./Compare_GBK.py genbank1.gbk genbank2.gbk output.fasta ortholog_list.csv")
-	
-	accession_fh = open(sys.argv[4], 'r');
-	fh1 = open(sys.argv[1], 'r')
-	fh2 = open(sys.argv[2], 'r')
-	gb1 = SeqIO.read(fh1, "genbank")
-	gb2 = SeqIO.read(fh2, "genbank")
+
+	parser = argparse.ArgumentParser(description = "")
+	parser.add_argument("-g", "--gbk-files", help="two genbank files", nargs=2, required=True)
+	parser.add_argument("-m", "--ortholog-map-file", help="ortholog mapping file", required=True)
+	parser.add_argument("-o", "--output-file", help="output fasta file name", required=True)
+
+	args = parser.parse_args()
+
+	accession_fh = open(args.ortholog_map_file, 'r');
+
+	gb1 = SeqIO.read(open(args.gbk_files[0], 'r'), "genbank")
+	gb2 = SeqIO.read(open(args.gbk_files[1], 'r'), "genbank")
 	
 	oh_shared_array = []
-
-	oh_shared_array.append(open(sys.argv[1] + ".shared.fasta", 'w'))
-	oh_shared_array.append(open(sys.argv[2] + ".shared.fasta", 'w'))
+	oh_shared_array.append(open(args.gbk_files[0] + ".shared.fasta", 'w'))
+	oh_shared_array.append(open(args.gbk_files[1] + ".shared.fasta", 'w'))
 
 	dic1 = {}
 	dic2 = {}
 
-	for feat in gb1.features:
-		if(feat.type == 'CDS'):
-			loc_split = feat.location.__str__()[1:-4].split(":")
-			loc_split[0] = str(int(loc_split[0]) + 1)
-			loc_split = loc_split[0] + ".." + loc_split[1]
-			dic1[loc_split] = {}
-			dic1[loc_split]['sequences'] = feat.qualifiers['translation'][0]
-			dic1[loc_split]['header'] = feat.qualifiers['locus_tag'][0]
-			dic1[loc_split]['in_both'] = False
-
-	for feat in gb2.features:
-		if(feat.type == 'CDS'):
-			loc_split = feat.location.__str__()[1:-4].split(":")
-			loc_split[0] = str(int(loc_split[0]) + 1)
-			loc_split = loc_split[0] + ".." + loc_split[1]
-			dic2[loc_split] = {}
-			dic2[loc_split]['sequences'] = feat.qualifiers['translation'][0]
-			dic2[loc_split]['header'] = feat.qualifiers['locus_tag'][0]
-			dic2[loc_split]['in_both'] = False
+	# load up our dictionary from our genbank files
+	for dic, genbank in [(dic1, gb1), (dic2, gb2)]:
+		for feat in genbank.features:
+			if(feat.type == 'CDS'):
+				loc_split = feat.location.__str__()[1:-4].split(":")
+				loc_split[0] = str(int(loc_split[0]) + 1)
+				loc_split = loc_split[0] + ".." + loc_split[1]
+				dic[loc_split] = {}
+				dic[loc_split]['sequences'] = feat.qualifiers['translation'][0]
+				dic[loc_split]['header'] = feat.qualifiers['locus_tag'][0]
+				dic[loc_split]['in_both'] = False
 
 
+	# load our comparison
 	comparison_list = []
 	r = csv.reader(accession_fh, delimiter=',', quotechar='|')
 
@@ -54,7 +51,7 @@ def main():
 		if(len(row) == 3) and all([len(r) > 0 for r in row]):
 			comparison_list.append([row[1], row[2]]);
 
-	# get ride of header
+	# get rid of header
 	comparison_list = comparison_list[1:]
 
 	# find orthologs
@@ -74,18 +71,12 @@ def main():
 				print "could not locate the accession numbers:" + com[x] + " in " + sys.argv[1+x]
 
 
-	with open(sys.argv[1] + ".fasta", 'w') as oh:
-		for gene in dic1.keys():
-			if(dic1[gene]['in_both'] != True):
-				oh.write(">" + dic1[gene]['header'] + "\n")
-				oh.write(dic1[gene]['sequences'] + "\n")
-
-	with open(sys.argv[2] + ".fasta", 'w') as oh:
-		for gene in dic2.keys():
-			if(dic2[gene]['in_both'] != True):
-				oh.write(">" + dic2[gene]['header'] + "\n")
-				oh.write(dic2[gene]['sequences'] + "\n")
-
+	for fn, dic in [(args.gbk_files[0], dic1), (args.gbk_files[1], dic2)]:
+		with open(fn + ".fasta", 'w') as oh:
+			for gene in dic.keys():
+				if(dic[gene]['in_both'] != True):
+					oh.write(">" + dic[gene]['header'] + "\n")
+					oh.write(dic[gene]['sequences'] + "\n")
 
 if __name__ == "__main__":
 	sys.exit(main())
